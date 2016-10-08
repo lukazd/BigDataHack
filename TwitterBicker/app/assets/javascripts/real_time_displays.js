db400aa06037f2f806f48065f1f84bf332dabd5b
stephaniek95 <stephaniek95@gmail.com>
2016-10-08 16:53:02 +0000

    both charts

--- TwitterBicker/app/assets/javascripts/real_time_displays.js
+++ TwitterBicker/app/assets/javascripts/real_time_displays.js
@@ -1,36 +1,47 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-console.log("barchart called");
-// Using requirejs
-
 
 window.onload = function(){
     var meanTweetChart = new barChart();
 };
+
 var barChart = function(){
+    this.candidates = {
+        HILLARY : 0,
+        TRUMP : 1,
+    }
     this.trumpBarChart = $("#ratio-chart-trump");
     this.hillaryBarChart = $("#ratio-chart-hillary");
-    this.createCharts();
+    this.createChart(this.candidates.TRUMP);
+    this.createChart(this.candidates.HILLARY);
 };
-barChart.prototype.test = function(){
-    console.log("test");
-};
-barChart.prototype.createCharts = function(){
-    console.log("create charts");
-    var trumpChart = new Chart(this.trumpBarChart, {
+barChart.prototype.createChart = function(candidate){
+    // these will be different for each candidate
+    var candidateEl, label, color;
+    if(candidate == 0){
+        candidateEl = this.hillaryBarChart;
+        label = 'Blue';
+        color = 'rgba(0, 0, 255, 1)';
+    }
+    else{
+        candidateEl = this.trumpBarChart;
+        label = 'Red';
+        color = 'rgba(255, 0, 0, 1)';
+    }
+    var chart = new Chart(candidateEl, {
     type: 'bar',
     data: {
-        labels: ["Red"],
+        labels: [label],
         datasets: [{
             label: '# of mean tweets',
             data: [50],
             backgroundColor: [
-                'rgba(255, 0, 0, 1)',
+                color
             ],
             borderColor: [
-                'rgba(255,0,0,1)',
+                color
             ],
-            borderWidth: 1
+            borderWidth: 0
         }]
     },
     options: {
