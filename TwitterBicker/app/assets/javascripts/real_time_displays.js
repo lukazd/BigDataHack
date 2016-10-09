17987b2768044a92c0d6077c65e3b724f6abc4c9
Luka Zdilar <luka-zdilar@uiowa.edu>
2016-10-09 00:03:55 +0000

    Add firebase stuff

--- TwitterBicker/app/assets/javascripts/real_time_displays.js
+++ TwitterBicker/app/assets/javascripts/real_time_displays.js
@@ -6,43 +6,57 @@ var $;
 var Chart; 
 var d3;
 
+var myFirebaseRef = new Firebase("https://twitterbicker.firebaseio.com/");
 window.onload = function(){
     Chart.defaults.global.tooltips = false;
     new barChart();
 };
+
+// Update the state of the sensor everytime that firebase updates 
+myFirebaseRef.child('Sensor').on('value', function(dataSnapshot) {
+    status = dataSnapshot.val();
+    if (status == false) {
+        $('#current-temperature-error').text("unplugged Sensor");
+        $('#current-temperature-error').show();
+        $('#current-temperature').hide();
+    } else {
+        initializeGraph();
+        $('#current-temperature').show();
+        $('#current-temperature-error').hide();
+    }
+});
+
 var candidates = {
     HILLARY : 0,
     TRUMP : 1,
 };
 var wordCloud = function(){
-    this.wordMapDiv = $("#word-map");
-    this.words =  [{text: "Lorem", weight: 13},
+    console.log("word cloud");
+    var words = [{text: "Lorem", weight: 13},
                 {text: "Ipsum", weight: 10.5},
                 {text: "Dolor", weight: 9.4},
                 {text: "Sit", weight: 8},
                 {text: "Amet", weight: 6.2},
                 {text: "Consectetur", weight: 5},
                 {text: "Adipiscing", weight: 5}];
-    this.create();
-};
-wordCloud.prototype.create = function(){
-     this.wordMapDiv.jQCloud(this.words, {
+    $('#word-map').jQCloud(words, {
             autoResize: true,
             //shape: 'rectangular',
-           width: 1500,
-           height: 500
+            width: 1500,
+            height: 500
     });
+};
+wordCloud.prototype.create = function(){
+    
 };
 wordCloud.prototype.update = function(){
     
 };
 var radarChart = function(){
-    this.hillaryData = [65, 59, 90, 81, 56];
-    this.trumpData = [28, 48, 40, 19, 96];
     this.radarDiv = $("#radar-chart-trump-hillary");
-    this.radarChart = this.create();
+    this.radarChart = this.createRadarChart();
 };
-radarChart.prototype.create = function(){
+radarChart.prototype.createRadarChart = function(){
     var data = {
         labels: ["Emotional Range", "Openness", "Conscientiousness", "Extraversion", "Agreeableness"],
         datasets: [
@@ -54,7 +68,7 @@ radarChart.prototype.create = function(){
                 pointBorderColor: "#fff",
                 pointHoverBackgroundColor: "#fff",
                 pointHoverBorderColor: "rgba(179,181,198,1)",
-                data: this.hillaryData
+                data: [65, 59, 90, 81, 56]
             },
             {
                 label: "Trump",
@@ -64,7 +78,7 @@ radarChart.prototype.create = function(){
                 pointBorderColor: "#fff",
                 pointHoverBackgroundColor: "#fff",
                 pointHoverBorderColor: "rgba(255,99,132,1)",
-                data: this.trumpData
+                data: [28, 48, 40, 19, 96]
             }
         ]
     };
@@ -74,70 +88,7 @@ radarChart.prototype.create = function(){
     });
     return myRadarChart;
 };
-var barChart = function(){
-    this.hillaryData = 50;
-    this.trumpData = 100;
-    this.trumpDiv = $("#ratio-chart-trump");
-    this.hillaryDiv = $("#ratio-chart-hillary");
-    // create the chart
-    this.trumpBarChart = this.create(candidates.TRUMP);
-    this.hillaryBarChart = this.create(candidates.HILLARY);
-};
-barChart.prototype.create = function(candidate){
-    // getting rid of the top legend
-    Chart.defaults.global.defaultFontSize = 20;
-    var maxData = 100;
-    // these will be different for each candidate
-    var candidateEl, label, color, data;
-    if(candidate == 0){
-        candidateEl = this.hillaryDiv;
-        color = 'rgba(0, 0, 255, 1)';
-        data = this.hillaryData;
-    }
-    else{
-        candidateEl = this.trumpDiv;
-        color = 'rgba(255, 0, 0, 1)';
-        data = this.trumpData;
-    }
-    var barChart = new Chart(candidateEl, {
-    type: 'bar',
-    data: {
-        labels: [data],
-        datasets: [{
-            label: data,
-            data: [data],
-            backgroundColor: [color],
-        }]
-    },
-    options: {
-        legend: {display:false, fontSize:40},
-        scales: {
-            yAxes: [{ 
-                display:false,
-                ticks: {
-                    beginAtZero:true,
-                    max:maxData
-                }
-                
-            }],
-            xAxes: [{
-                gridLines : {
-                    display : false
-                },
-            }]
-            
-        }
-    }});
-    return barChart;
-};
-barChart.prototype.update = function(){
-   this.trumpBarChart.updateChart();
-   this.hillaryBarChart.updateChart();
-};
-barChart.prototype.update = function(){
-   this.trumpBarChart.updateChart();
-   this.hillaryBarChart.updateChart();
-};
+
 function changeTab(option){
     $('#ratio-charts').css('display', 'none');
     $('#radar-chart').css('display', 'none');
@@ -151,9 +102,68 @@ function changeTab(option){
         $('#radar-chart').css('display', 'block');
         new radarChart();
     } else {
+        //$('#word-map').css('display', 'block');
         $('#word-map').fadeIn("slow");
         new wordCloud();
     }
     
     return false;
+};
+var barChart = function(){
+    this.trumpDiv = $("#ratio-chart-trump");
+    this.hillaryDiv = $("#ratio-chart-hillary");
+    // create the chart
+    this.trumpBarChart = this.createBarChart(candidates.TRUMP);
+    this.hillaryBarChart = this.createBarChart(candidates.HILLARY);
+};
+barChart.prototype.createBarChart = function(candidate){
+    // getting rid of the top legend
+    Chart.defaults.global.defaultFontSize = 20;
+    var maxData = 100;
+    // these will be different for each candidate
+    var candidateEl, label, color, data;
+    if(candidate == 0){
+        candidateEl = this.hillaryDiv;
+        color = 'rgba(0, 0, 255, 1)';
+        data = 20;
+    }
+    else{
+        candidateEl = this.trumpDiv;
+        color = 'rgba(255, 0, 0, 1)';
+        data = 100;
+    }
+    var barChart = new Chart(candidateEl, {
+    type: 'bar',
+    data: {
+        labels: [data],
+        datasets: [{
+            label: data,
+            data: [data],
+            backgroundColor: [color],
+        }]
+    },
+    options: {
+        legend: {display:false, fontSize:40},
+        //labels: {fontSize:20},
+        scales: {
+            yAxes: [{
+                display:false,
+                ticks: {
+                    beginAtZero:true,
+                    max:maxData
+                }
+            }],
+            xAxes: [{
+                gridLines : {
+                    display : false
+                },
+                
+            }]
+        }
+    }});
+    return barChart;
+};
+barChart.prototype.updateChart = function(){
+   this.trumpBarChart.updateChart();
+   this.hillaryBarChart.updateChart();
 };
